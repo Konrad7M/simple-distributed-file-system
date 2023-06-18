0958b61225d69e1241b24c37592f1604ac23b8c0
Konrad Matusiak <matusiak7konrad@gmail.com>
2023-06-18 22:43:33 +0200

    constants folder

---
+++ constants/constants.go
@@ -0,0 +1,8 @@
+package constants
+
+const (
+	dataNodeClientListenerPort       = 8085
+	dataNodeHealthCheckListenerPort  = 8086
+	metaDataNodeBlockListeningPort   = 8081
+	metaDataNodeBlockHealthCheckPort = 8087
+)
